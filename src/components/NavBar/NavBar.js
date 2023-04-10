39d0bafd779ea9b32f38e543693f6e8dcab14a51
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-04-10 20:59:43 +0200

    Bug Fix
    Fixed an issue where clicking on the website title div would navigate to a blank screen

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
 		<>
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
 				<div className="container">
-					<Link className="navbar-brand" to="/">Algorithm Visualizer</Link>
+					<Link className="navbar-brand" to="/algorithm-visualizer">Algorithm Visualizer</Link>
 					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>
