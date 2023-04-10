8ff41911b8916b10e50c6df3d2131c2dbaa429ae
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-04-10 20:48:26 +0200

    Improvements to the featured section

--- src/components/FeaturedAlgorithms/FeaturedAlgorithms.js
+++ src/components/FeaturedAlgorithms/FeaturedAlgorithms.js
@@ -5,38 +5,43 @@ import './FeaturedAlgorithms.css';
 const FeaturedAlgorithms = () => {
 	const algorithms = [
 		{
-			id: 'bubble-sort',
-			title: 'Bubble Sort',
-			description: 'A simple sorting algorithm that repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order.',
-			image: require('../../assets/Algorithms/bubble-sort.jpg')
+			id: 'sorting-algorithms',
+			title: 'Sorting Algorithms',
+			description: 'Discover the fascinating realm of sorting algorithms through our engaging visualizers. Experience the magic of popular algorithms like Bubble Sort, Quick Sort, and Merge Sort, as they efficiently organize data in a specific order. Unravel the intricacies of these classic algorithms and enhance your understanding of their underlying principles.',
+			image: require('../../assets/Algorithms/sorting-algorithms-image.png')
 
 		},
 		{
-			id: 'dijkstras',
-			title: 'Dijkestra\'s Algorithm',
-			description: 'A graph search algorithm that solves the single-source shortest path problem for a graph with non-negative edge weights, producing a shortest-path tree.',
-			image: require('../../assets/Algorithms/dijkstras.jpg')
+			id: 'pathfinding-algorithms',
+			title: 'Path Finding Algorithms',
+			description: 'Explore the world of path finding algorithms with our interactive visualizers. Dive into popular algorithms such as A* and Dijkstra\'s, and learn how they efficiently find the shortest path between two points in a graph. Unlock the secrets of these powerful algorithms and gain a deeper understanding of their inner workings.',
+			image: require('../../assets/Algorithms/pathfinding-algorithms-image.jpg')
 		},
 	];
 
 	return (
 		<div className="featured-algorithms container mt-5">
-			<h2 className="text-center mb-4">Featured Algorithms</h2>
 			<div className="row">
 				{algorithms.map(algorithm => (
-					<div className="col-md-4" key={algorithm.id}>
+					<div className="col-md-12 col-lg-11" key={algorithm.id}>
 						<div className="card mb-4">
-							<img className="card-img-top" src={algorithm.image} alt={algorithm.title} />
-							<div className="card-body">
-								<h3 className="card-title">{algorithm.title}</h3>
-								<p className="card-text">{algorithm.description}</p>
-								<Link className="btn btn-primary" to={`/algorithms/${algorithm.id}`}>View Algorithm</Link>
+							<div className="row g-0" >
+								<div className="col-md-4">
+									<img className="card-img" src={algorithm.image} alt={algorithm.title} />
+								</div>
+								<div className="col-md-8" >
+									<div className="card-body">
+										<h3 className="card-title">{algorithm.title}</h3>
+										<p className="card-text">{algorithm.description}</p>
+										<Link className="btn btn-primary" to={`/algorithms/${algorithm.id}`}>Explore Visualizers</Link>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				))}
 			</div>
-		</div>
+		</div >
 	);
 };
 
