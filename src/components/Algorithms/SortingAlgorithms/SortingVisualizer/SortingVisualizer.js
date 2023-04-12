1e0dab891aafe3d34e9e661963aa848a6066671f
Youssef Albokl <88749793+Boklino@users.noreply.github.com>
2023-04-12 21:15:44 +0200

    fixed array margin problem

--- src/components/Algorithms/SortingAlgorithms/SortingVisualizer/SortingVisualizer.js
+++ src/components/Algorithms/SortingAlgorithms/SortingVisualizer/SortingVisualizer.js
@@ -4,15 +4,21 @@ import useD3 from '../../../../hooks/useD3';
 import './SortingVisualizer.css';
 import SideBar from '../../../SideBar/SideBar';
 import AlgoCompass from '../../../AlgoCompass/AlgoCompass';
-import { BUBBLE_SORT_DESCRIPTION, MERGE_SORT_DESCRIPTION, HEAP_SORT_DESCRIPTION, QUICK_SORT_DESCRIPTION } from '../../../../assets/Algorithms/strings';
-
+import {
+	BUBBLE_SORT_DESCRIPTION,
+	MERGE_SORT_DESCRIPTION,
+	HEAP_SORT_DESCRIPTION,
+	QUICK_SORT_DESCRIPTION,
+} from '../../../../assets/Algorithms/strings';
 
 const SortingVisualizer = () => {
 	const [array, setArray] = useState([]);
 	const [selectedAlgorithm, setSelectedAlgorithm] = useState(null);
 
 	const generateRandomArray = () => {
-		const newArray = Array.from({ length: 100 }, () => Math.floor(Math.random() * (1000 - 20 + 1) + 20));
+		const newArray = Array.from({ length: 100 }, () =>
+			Math.floor(Math.random() * (1000 - 20 + 1) + 20)
+		);
 		setArray(newArray);
 	};
 
@@ -20,31 +26,36 @@ const SortingVisualizer = () => {
 		generateRandomArray();
 	}, []);
 
-	const ref = useD3((svg) => {
-		const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-		const width = 960 - margin.left - margin.right;
-		const height = 500 - margin.top - margin.bottom;
+	const ref = useD3(
+		(svg) => {
+			const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+			const width = 960 - margin.left - margin.right;
+			const height = 500 - margin.top - margin.bottom;
 
-		svg.selectAll('*').remove();
+			svg.selectAll('*').remove();
 
-		const x = d3.scaleBand().range([0, width]).padding(0.3);
-		const y = d3.scaleLinear().range([height, 0]);
+			const x = d3.scaleBand().range([0, width]).padding(0.3);
+			const y = d3.scaleLinear().range([height, 0]);
 
-		const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
+			const g = svg
+				.append('g')
+				.attr('transform', `translate(${margin.left},${margin.top})`);
 
-		x.domain(array.map((_, i) => i));
-		y.domain([0, d3.max(array)]);
+			x.domain(array.map((_, i) => i));
+			y.domain([0, d3.max(array)]);
 
-		g.selectAll('.bar')
-			.data(array)
-			.enter()
-			.append('rect')
-			.attr('class', 'bar')
-			.attr('x', (_, i) => x(i))
-			.attr('y', (d) => y(d))
-			.attr('width', x.bandwidth())
-			.attr('height', (d) => height - y(d));
-	}, [array]);
+			g.selectAll('.bar')
+				.data(array)
+				.enter()
+				.append('rect')
+				.attr('class', 'bar')
+				.attr('x', (_, i) => x(i))
+				.attr('y', (d) => y(d))
+				.attr('width', x.bandwidth())
+				.attr('height', (d) => height - y(d));
+		},
+		[array]
+	);
 
 	// Algorithms data
 	const algorithms = [
@@ -60,11 +71,25 @@ const SortingVisualizer = () => {
 	};
 
 	return (
-		<div className="sorting-visualizer">
-			<SideBar algorithms={algorithms} onSelectAlgorithm={handleSelectAlgorithm} onGenerateNewArray={generateRandomArray} />
-			<div className="visualizer-container">
-				<svg ref={ref} width="960" height="500" className="sorting-visualizer-svg"></svg>
-				{selectedAlgorithm && <AlgoCompass algorithm={selectedAlgorithm} />}
+		<div className='sorting-visualizer'>
+			<SideBar
+				algorithms={algorithms}
+				onSelectAlgorithm={handleSelectAlgorithm}
+				onGenerateNewArray={generateRandomArray}
+			/>
+
+			<div className='visualizer-container'>
+				<svg
+					ref={ref}
+					width='960'
+					height='500'
+					className='sorting-visualizer-svg'
+				></svg>
+				<div className='visualizer-container-text'>
+					{selectedAlgorithm && (
+						<AlgoCompass algorithm={selectedAlgorithm} />
+					)}
+				</div>
 			</div>
 		</div>
 	);
