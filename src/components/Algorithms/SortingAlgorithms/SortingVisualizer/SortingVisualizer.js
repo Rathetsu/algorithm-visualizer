dbebdcb77af3cd2e6678fdb3554848814ed24a2c
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-04-17 02:48:55 +0200

    Implemented Animations

--- src/components/Algorithms/SortingAlgorithms/SortingVisualizer/SortingVisualizer.js
+++ src/components/Algorithms/SortingAlgorithms/SortingVisualizer/SortingVisualizer.js
@@ -10,6 +10,7 @@ import { mergeSort, quickSort, heapSort, bubbleSort } from '../SortingAlgorithms
 const SortingVisualizer = () => {
 	const [array, setArray] = useState([]);
 	const [selectedAlgorithm, setSelectedAlgorithm] = useState(null);
+	// const [sorting, setSorting] = useState(false);
 
 	const generateRandomArray = () => {
 		let max = 1000
@@ -18,18 +19,70 @@ const SortingVisualizer = () => {
 		setArray(newArray);
 	};
 
+	const startSorting = () => {
+		// setSorting(true);
+		animateSorting(selectedAlgorithm.function, array, 50)
+			.then((sortedArray) => {
+				setArray(sortedArray);
+				// setSorting(false);
+			})
+			.catch((error) => {
+				console.error('Error during sorting:', error);
+				// setSorting(false);
+			});
+	};
+
 	const animateSorting = (steps, animationSpeed = 50) => {
-		steps.forEach((step, index) => {
-			setTimeout(() => {
-				if (step.type === 'compare') {
-					// Animate the comparison
-					// Change the color of the two bars being compared
-				} else if (step.type === 'swap') {
-					// Animate the swap
-					// Swap the two bars and reset their colors
-				}
-			}, index * animationSpeed);
-		});
+		console.log(steps)
+		if (steps.length === 0) return;
+
+		const svg = d3.select(ref.current);
+		const bars = svg.selectAll('.bar');
+
+		const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+		// const width = 960 - margin.left - margin.right;
+		const height = 500 - margin.top - margin.bottom;
+
+		const y = d3.scaleLinear().range([height, 0]);
+		y.domain([0, d3.max(array)]);
+
+		let i = 0;
+		const interval = setInterval(() => {
+			const step = steps[i];
+
+			if (step.type === 'compare') {
+				bars
+					.filter((_, idx) => idx === step.first || idx === step.second)
+					.transition()
+					.duration(animationSpeed / 2)
+					.style('fill', 'red');
+			} else if (step.type === 'swap') {
+				// Swap the array values
+				[array[step.first], array[step.second]] = [array[step.second], array[step.first]];
+
+				// Update the bar positions
+				bars
+					.data(array)
+					.transition()
+					.duration(animationSpeed / 2)
+					.attr('y', (d) => y(d))
+					.attr('height', (d) => height - y(d))
+					.style('fill', 'steelblue');
+			}
+
+			i++;
+
+			if (i >= steps.length) {
+				clearInterval(interval);
+
+				// Reset bar colors after sorting is finished
+				bars
+					.transition()
+					.duration(animationSpeed / 2)
+					.delay((_, idx) => idx * 10)
+					.style('fill', 'steelblue');
+			}
+		}, animationSpeed);
 	};
 
 	useEffect(() => {
@@ -69,17 +122,19 @@ const SortingVisualizer = () => {
 
 	// Algorithms data
 	const algorithms = [
-		{ id: 1, name: 'Bubble Sort', description: strings.BUBBLE_SORT_DESCRIPTION },
-		{ id: 2, name: 'Merge Sort', description: strings.MERGE_SORT_DESCRIPTION },
-		{ id: 3, name: 'Quick Sort', description: strings.QUICK_SORT_DESCRIPTION },
-		{ id: 4, name: 'Heap Sort', description: strings.HEAP_SORT_DESCRIPTION },
+		{ id: 1, name: 'Bubble Sort', description: strings.BUBBLE_SORT_DESCRIPTION, function: bubbleSort },
+		{ id: 2, name: 'Merge Sort', description: strings.MERGE_SORT_DESCRIPTION, function: mergeSort },
+		{ id: 3, name: 'Quick Sort', description: strings.QUICK_SORT_DESCRIPTION, function: quickSort },
+		{ id: 4, name: 'Heap Sort', description: strings.HEAP_SORT_DESCRIPTION, function: heapSort },
 	];
 
-	const handleSelectAlgorithm = (algorithm) => {
-		setSelectedAlgorithm(algorithm);
-		const steps = algorithm.function(array);
-		animateSorting(steps);
+	const handleSelectAlgorithm = (algorithmId) => {
+		const selectedAlgo = algorithms.find((algo) => algo.id === algorithmId);
+		setSelectedAlgorithm(selectedAlgo);
+		// const sortingSteps = selectedAlgo.function(array.slice());
+		// animateSorting(sortingSteps, 5);
 	};
+
 
 
 	return (
@@ -104,8 +159,18 @@ const SortingVisualizer = () => {
 						</div>
 					)}
 				</div>
-			</div>
+				<div className="sorting-button-container">
+					<button
+						className='`btn sorting-button ${!selectedAlgorithm ? "disabled" : ""}`'
+						onClick={startSorting}
+						disabled={!selectedAlgorithm}
+						style={{ alignSelf: "center" }}
+					>
+						Start Sorting
+					</button>
+				</div>
 
+			</div>
 		</div>
 	);
 };
